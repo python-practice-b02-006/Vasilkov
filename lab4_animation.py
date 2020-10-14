b9631594decb0fdd2ee840c32b0f51c70ca4faaf
Артем <vasilkov.aa@phystech.edu>
2020-10-15 00:01:13 +0300

    fixed

--- lab4_animation.py
+++ lab4_animation.py
@@ -1,123 +1,150 @@
 import pygame
 import numpy as np
 import datetime
-##
+
 
 screen = pygame.display.set_mode([1000,800])
-
-now = datetime.datetime.now()
-
 clock = pygame.time.Clock()
+
+
 def u(x, y):
-    pygame.draw.polygon(screen,(255, 0, 0), [(x-30-10, y), (x-30, y-10),
-                                             (x+30,y-10), (x+30+10, y), (x+30, y+10), (x-30, y+10)])
+    pygame.draw.polygon(screen, (255, 0, 0), [(x-30-10, y), (x-30, y-10),
+                                (x+30, y-10), (x+30+10, y), (x+30, y+10), (x-30, y+10)])
+
+
 def ru(x, y):
     pygame.draw.polygon(screen, (255, 0, 0), [(x, y - 30 - 10), (x - 10, y - 30),
-                                              ( x - 10, y + 30), ( x, y + 30 + 10), (x+10, y + 30), (x+10, y - 30)])
-def sqs(x ,y):
+                                (x - 10, y + 30), ( x, y + 30 + 10), (x+10, y + 30), (x+10, y - 30)])
+
+
+def sqs(x, y):
     pygame.draw.rect(screen, (255, 0, 0), [x-20, y-50, 20 ,20])
     pygame.draw.rect(screen, (255, 0, 0), [x - 20, y+30, 20, 20])
+
+
 def one(x, y):
-    ru(x+46 ,y+46)
+    ru(x+46, y+46)
     ru(x+46, y-46)
-def two(x ,y):
+
+
+def two(x, y):
     u(x, y)
     ru(x+46, y-46)
     u(x, y-92)
     ru(x-46, y+46)
     u(x, y+92)
+
+
 def three(x, y):
     u(x, y)
     ru(x + 46, y - 46)
     u(x, y - 92)
     ru(x + 46, y + 46)
     u(x, y + 92)
-def four(x ,y):
+
+
+def four(x, y):
     u(x, y)
     ru(x - 46, y - 46)
     ru(x + 46, y - 46)
     ru(x + 46, y + 46)
+
+
 def five(x, y):
     u(x, y)
     ru(x - 46, y - 46)
     u(x, y - 92)
     ru(x + 46, y + 46)
     u(x, y + 92)
-def six(x ,y):
+
+
+def six(x, y):
     u(x, y)
     ru(x - 46, y - 46)
     u(x, y - 92)
     ru(x + 46, y + 46)
     ru(x - 46, y + 46)
     u(x, y + 92)
+
+
 def seven(x, y):
     u(x, y - 92)
     one(x, y)
+
+
 def eight(x, y):
     six(x, y)
     ru(x+46, y-46)
+
+
 def nine(x, y):
     five(x, y)
     ru(x+46, y-46)
-def zero(x ,y):
-    seven(x ,y)
+
+
+def zero(x, y):
+    seven(x, y)
     one(x-92, y)
     u(x, y+92)
 
-def CClocks(T, x ,y):
-    t=T%10
-    if t==0:
+
+def cclocks (T, x, y):
+
+    t = T % 10
+    if t == 0:
         zero(x, y)
-    if t==1:
+    if t == 1:
        one(x, y)
-    if t==2:
+    if t == 2:
         two(x, y)
-    if t==3:
+    if t == 3:
         three(x, y)
-    if t==4:
+    if t == 4:
         four(x, y)
-    if t==5:
+    if t == 5:
         five(x, y)
-    if t==6:
+    if t == 6:
         six(x, y)
-    if t==7:
-        seven(x ,y)
-    if t==8:
-        eight(x ,y)
-    if t==9:
-        nine(x ,y)
-    z=(T-T%10)/10
-    if z==0:
+    if t == 7:
+        seven(x, y)
+    if t == 8:
+        eight(x, y)
+    if t == 9:
+        nine(x, y)
+    z = (T-T % 10)/10
+    if z == 0:
         zero(x-120, y)
         return
-    if z==1:
+    if z == 1:
        one(x-120, y)
        return
-    if z==2:
+    if z == 2:
         two(x-120, y)
         return
-    if z==3:
+    if z == 3:
         three(x-120, y)
         return
-    if z==4:
+    if z == 4:
         four(x-120, y)
         return
-    if z==5:
+    if z == 5:
         five(x-120, y)
         return
-    if z==6:
+    if z == 6:
         six(x-120, y)
         return
-    if z==7:
-        seven(x-120 ,y)
+    if z == 7:
+        seven(x-120, y)
         return
-    if z==8:
+    if z == 8:
         eight(x-120 ,y)
         return
-    if z==9:
-        nine(x-120 ,y)
+    if z == 9:
+        nine(x-120, y)
         return
-done=False
+
+
+done = False
 pygame.display.set_caption("wtf")
 
 while not done:
@@ -125,16 +152,16 @@ while not done:
     clock.tick(1)
     Now = datetime.datetime.now()
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            done=True
+        if event.type == pygame.QUIT:
+            done = True
     screen.fill((0, 0, 0))
-    if(Now.second%2==0):
+    if Now.second % 2 == 0:
         sqs(650, 400)
         sqs(350, 400)
-    CClocks(Now.second, 850, 400)
+    cclocks(Now.second, 850, 400)
 
-    CClocks(Now.minute, 550, 400)
-    CClocks(Now.hour, 250, 400)
+    cclocks(Now.minute, 550, 400)
+    cclocks(Now.hour, 250, 400)
     pygame.display.flip()
 
 pygame.quit()
